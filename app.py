926ed9a1df813d2ac42c9f5e53024ba10ec460ef
Glen <glencabusas@gmail.com>
2023-10-13 22:50:23 +1100

    Refactored much of the code with better code practices. Added AMZN functionality. Added login page and login requirements. Currently in working order and deployed.

--- app.py
+++ app.py
@@ -1,28 +1,71 @@
-from flask import Flask, jsonify, request, render_template,send_from_directory
+
+from flask import Flask, flash, session, jsonify, request, render_template,send_from_directory,redirect, url_for
 import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flask_login import LoginManager, UserMixin, login_user, login_required
+from flask_bcrypt import Bcrypt
+import config
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:passwordHERE@localhost:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = config.database_uri
+app.secret_key = config.secret_key
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(user_id):
+    user = User()
+    user.id = user_id
+    return user
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = 'admin'
+        password = request.form["password"]
+        next_url = request.form["next"]
+        if username == config.USERNAME and bcrypt.check_password_hash(config.PASSWORD, password):
+            user = User()
+            user.id = username
+            login_user(user)
+            session.permanent = True
+            if next_url:
+                return redirect(next_url)
+            return redirect(url_for("index"))
+        else:
+            flash("Incorrect username or password!")
+    return render_template("login.html")
+
 @app.route('/')
+@login_required
 def index():
+    return redirect(url_for("costco"))
+
+@app.route('/costco')
+@login_required
+def costco():
     return render_template('searcher.html')
 
 @app.route('/image/<path:filename>', methods=['GET'])
+@login_required
 def serve_image(filename):
-    print(f"Trying to serve: {filename}")
     return send_from_directory(os.path.dirname(filename), os.path.basename(filename))
 
-
-@app.route('/search-part', methods=['GET'])
+@app.route('/costco/search-part', methods=['GET'])
+@login_required
 def search_part():
     query = request.args.get('query')
     with db.engine.connect() as connection:
         results = connection.execute(
-            text("SELECT p.part_number, p.description, i.image_path FROM product p, images i WHERE i.part_number = p.part_number AND p.part_number ILIKE :search_query || '%'").bindparams(search_query=query)
+            text("SELECT p.part_number, p.description, i.image_path FROM product p, images i WHERE i.part_number = p.part_number AND p.part_number ILIKE :search_query || '%' ORDER BY p.part_number ASC").bindparams(search_query=query)
         ).fetchall()
     
     return jsonify([{
@@ -31,8 +74,8 @@ def search_part():
         'image': item[2]
     } for item in results])
 
-
-@app.route('/search', methods=['GET'])
+@app.route('/costco/search', methods=['GET'])
+@login_required
 def search():
     query = request.args.get('query')
     with db.engine.connect() as connection:
@@ -43,10 +86,11 @@ def search():
             FROM product p, images i 
             WHERE i.part_number = p.part_number 
             AND ({search_conditions})
+            ORDER BY p.part_number ASC;
         """
         params = {f"term{i}": term for i, term in enumerate(terms)}
         results = connection.execute(text(sql).bindparams(**params)).fetchall()
-
+    
     return jsonify([{
         'part_number': item[0],
         'description': item[1],
@@ -54,4 +98,46 @@ def search():
     } for item in results])
 
 
+@app.route('/amazon')
+@login_required
+def amazon():
+    return render_template('amazon_searcher.html')
 
+@app.route('/amazon/search-part', methods=['GET'])
+@login_required
+def amazon_search_part():
+    query = request.args.get('query')
+    with db.engine.connect() as connection:
+        results = connection.execute(
+            text("SELECT p.part_number, p.description, i.image_path FROM amazon_product p, amazon_images i WHERE i.part_number = p.part_number AND p.part_number ILIKE :search_query || '%' ORDER BY p.part_number ASC").bindparams(search_query=query)
+        ).fetchall()
+    
+    return jsonify([{
+        'part_number': item[0],
+        'description': item[1],
+        'image': item[2]
+    } for item in results])
+
+@app.route('/amazon/search', methods=['GET'])
+@login_required
+def amazon_search():
+    query = request.args.get('query')
+    with db.engine.connect() as connection:
+        terms = query.split()
+        search_conditions = ' AND '.join([f"(p.description ILIKE '%%' || :term{i} || '%%')" for i, term in enumerate(terms)])
+        sql = f"""
+            SELECT p.part_number, p.description, i.image_path 
+            FROM amazon_product p, amazon_images i 
+            WHERE i.part_number = p.part_number 
+            AND ({search_conditions})
+            ORDER BY p.part_number ASC
+        """
+        params = {f"term{i}": term for i, term in enumerate(terms)}
+        results = connection.execute(text(sql).bindparams(**params)).fetchall()
+    
+
+    return jsonify([{
+        'part_number': item[0],
+        'description': item[1],
+        'image': item[2]
+    } for item in results])
---
+++ templates/amazon_script.js
@@ -0,0 +1,230 @@
+let grouped = {};
+        
+// Image Zooming 
+function openImageModal(partNumber, clickedImageIndex = 0) {
+    const imageList = grouped[partNumber].images;
+    const enlargedImage = document.getElementById('enlargedImage');
+    enlargedImage.src = `/image/${imageList[clickedImageIndex].replace(/\\/g, '/')}`;
+    enlargedImage.dataset.currentImageIndex = clickedImageIndex; 
+    enlargedImage.dataset.partNumber = partNumber; 
+    $('#imageModal').modal('show');
+}
+
+document.getElementById('prevImage').addEventListener('click', function(e) {
+    e.preventDefault();
+    switchModalImage(-1);
+});
+
+document.getElementById('nextImage').addEventListener('click', function(e) {
+    e.preventDefault();
+    switchModalImage(1);
+});
+
+function switchModalImage(direction) {
+    const imgElement = document.getElementById('enlargedImage');
+    const currentImageIndex = +imgElement.dataset.currentImageIndex; 
+    const partNumber = imgElement.dataset.partNumber;
+    const imageList = grouped[partNumber].images;
+
+    let newIndex = currentImageIndex + direction;
+    if (newIndex < 0) newIndex = imageList.length - 1;  
+    if (newIndex >= imageList.length) newIndex = 0;     
+
+    imgElement.src = `/image/${imageList[newIndex].replace(/\\/g, '/')}`;
+    imgElement.dataset.currentImageIndex = newIndex;    
+
+}
+function closeImageModal() {
+    $('#imageModal').modal('hide');
+}
+
+function copy(element){
+    const tempInput = document.createElement('input');
+    tempInput.value = element.previousElementSibling.textContent.trim();
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    
+    const tooltip = element.nextElementSibling;
+    tooltip.style.visibility = "visible";
+    tooltip.style.opacity = "1";
+    setTimeout(() => {
+        tooltip.style.opacity = "0";
+    }, 1500); 
+}
+
+function handleKeyPress(event) {
+    if (event.keyCode === 13 && event.target.id === 'searchQuery') {
+        search();
+    }
+}
+
+function search() {
+    const query = document.getElementById("searchQuery").value;
+    if (query === "") {
+        return;
+    }
+    const searchType = document.querySelector('input[name="searchType"]:checked').value;
+
+    let endpoint;
+    if (searchType === "part") {
+        endpoint = `/amazon/search-part?query=${query}`;
+    } else {
+        endpoint = `/amazon/search?query=${query}`;
+    }
+
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        let uniquePartNumbers = new Set(data.map(item => item.part_number));
+        document.getElementById("resultsCount").innerText = `Total Results: ${uniquePartNumbers.size}`;
+        grouped = {};
+        data.forEach(item => {
+            
+            if (!grouped[item.part_number]) {
+                grouped[item.part_number] = {
+                    description: item.description,
+                    images: []
+                };
+            }
+            grouped[item.part_number].images.push(item.image);
+        });
+
+        const displayType = document.getElementById("displayType").value;
+        if (displayType === 'grid') {
+            let html = '<div class="row">'; 
+
+            for (const partNumber in grouped) {
+                const item = grouped[partNumber];
+                
+                html += `<div class="col-md-3 mb-4">`;  
+                html += `<div class="image-card">`;
+
+                html += `<div id="carousel${partNumber}" class="carousel slide" data-ride="carousel">`;
+                html += `<div class="carousel-inner">`;
+
+                item.images.forEach((imgSrc, index) => {
+                    html += `
+                    <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                        <img src="/image/${imgSrc.replace(/\\/g, '/')}" class="d-block w-100" cursor: pointer;" onclick="openImageModal('${partNumber}', ${index})" alt="Image for ${partNumber}">
+                    </div>`;
+                });
+
+                html += `</div>`; 
+
+                if (item.images.length > 1) {  
+                    html += `
+                    <a class="carousel-control-prev" href="#carousel${partNumber}" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel${partNumber}" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>`;
+                }
+                
+                html += `</div>`;  
+                html += `<p class="copy-container">
+                        <strong>${partNumber}</strong>
+                        <span class="material-icons copy-icon" onclick="copy(this)">content_copy</span>
+                        <span class="tooltip">Copied!</span>
+                    </p>`;
+    
+                html += `<p style="font-size: 0.8rem;">${item.description}</p>`;
+
+                html += `</div>`; 
+                html += `</div>`;  
+            }
+
+            html += '</div>';  
+            document.getElementById("results").innerHTML = html;
+
+        } else { 
+            // Table section
+            let html = '<table class="table table-bordered">';  
+            html += '<thead>';
+            html += '<tr>';
+            html += '<th class="part-number-column">Part Number</th>';
+            html += '<th class="text-center">Description</th>';
+            html += '<th class="text-center">Image</th>';
+            html += '</tr>';
+            html += '</thead>';
+            html += '<tbody>';  
+
+            for (const partNumber in grouped) {
+                const item = grouped[partNumber];
+                html += `<tr>`;  
+                html += `<td class="copy-table-cell">
+                    <span class="part-number">${partNumber}</span>
+                    <i class="material-icons copy-icon" onclick="copy(this)">content_copy</i>
+                    <span class="tooltip-text">Copied!</span>
+                    </td>`;  
+                html += `<td>${item.description}</td>`;  
+
+                html += `<td style="position: relative; width: 200px;">`; 
+                html += `<div id="carousel${partNumber}" class="carousel slide" data-ride="carousel">`;
+                html += `<div class="carousel-inner">`;
+
+                item.images.forEach((imgSrc, index) => {
+                    html += `
+                    <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                        <img src="/image/${imgSrc.replace(/\\/g, '/')}" class="d-block w-100" style="max-width: 100%; max-height: 150px; object-fit: contain;" cursor: pointer;" onclick="openImageModal('${partNumber}', ${index})" alt="Image for ${partNumber}">
+                    </div>`;
+                });
+
+                html += `</div>`;
+                if (item.images.length > 1) { 
+                    html += `<a class="carousel-control-prev" href="#carousel${partNumber}" role="button" data-slide="prev" style="position: absolute; top: 50%; transform: translateY(-50%); left: -10px; background: rgba(255, 255, 255, 0.5); z-index: 1;">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel${partNumber}" role="button" data-slide="next" style="position: absolute; top: 50%; transform: translateY(-50%); right: -10px; background: rgba(255, 255, 255, 0.5); z-index: 1;">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>`;
+                }
+
+                html += `</div>`;
+                html += `</td>`; 
+                html += `</tr>`; 
+            }
+
+            html += '</tbody>'; 
+            html += '</table>'
+
+            
+           
+                    
+            document.getElementById("results").innerHTML = html;
+        }
+        window.scrollTo({top: 0});
+        
+    
+    });
+}
+
+const searchbar = document.getElementById('search-section');
+let ogcontainer = document.getElementById('upper-container');
+let originalwidth = getComputedStyle(ogcontainer).width;
+const stickyPoint = searchbar.getBoundingClientRect().bottom + searchbar.ownerDocument.defaultView.pageYOffset;
+window.addEventListener('scroll',
+function() {
+    if (window.pageYOffset > stickyPoint) {
+        searchbar.classList.add('sticky');
+        searchbar.style.width = originalwidth;
+    } else {
+        searchbar.classList.remove('sticky');
+        searchbar.style.width = ''
+    }
+
+})
+
+window.addEventListener('resize',
+function() {
+    originalwidth = getComputedStyle(ogcontainer).width;
+    if (searchbar.classList.contains('sticky')) {
+        searchbar.style.width = originalwidth;
+    }
+})
